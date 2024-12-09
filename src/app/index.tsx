cadcd6773df8d0f5ffd1a45ce31db7e9266fe04e
Sinan Öztürk <sinanozturkk@icloud.com>
2024-12-09 20:55:20 +0300

    initialize Expo project with TypeScript, Tailwind CSS, and custom components

---
+++ src/app/index.tsx
@@ -0,0 +1,6 @@
+import DOMTest from "@/components/DOMTest";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return <DOMTest />;
+}
---
+++ src/components/DOMTest.tsx
@@ -0,0 +1,42 @@
+"use dom";
+import "../../global.css";
+import { IS_DOM } from "expo/dom";
+import { Button } from "./ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const DOMTest = () => {
+  return (
+    <div>
+      <Button>TEST</Button>
+
+      <AlertDialog>
+        <AlertDialogTrigger>Open</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default DOMTest;
